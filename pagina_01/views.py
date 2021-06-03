681514ef2d71217a5e4d239da0ee040e573ce8e7
JeanContreras12 <j.contrerasmanriquez@uandresbello.edu>
2021-06-03 13:11:35 -0400

    Registro adm

--- pagina_01/views.py
+++ pagina_01/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .forms import CustomUserForm
+from django.contrib.auth import login, authenticate
 # Create your views here.
 
 def login(request):
     return render(request, 'pagina_01/logeado.html')
 
 def registro(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data["username"]
-    else:
-        form = UserCreationForm()
-    context = { 'form' : form}
-    return render(request, 'pagina_01/registro.html')
+    data={
+        'form':CustomUserForm()
+    }
+    if request.method == 'POST':
+        formulario = CustomUserForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            #autenticar al usuario y redirigir al inicio
+            username=formulario.cleaned_data['username']
+            password=formulario.cleaned_data['password1']
+            user=authenticate(username=username,password=password)
+            login(request)
+            return redirect(to='inicio')
+
+    return render(request, 'pagina_01/registro.html',data)
