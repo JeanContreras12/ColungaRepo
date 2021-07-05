c3b5a1d22cd868db30e93512e08efc24809fb1c8
Matiasmillan <62578831+Matiasmillan@users.noreply.github.com>
2021-07-05 16:05:50 -0400

    Perfil Editable
    Es posible extraer datos de la BD y modificar los datos

--- pagina_01/forms.py
+++ pagina_01/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
@@ -9,3 +9,20 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ['username', 'first_name', 'last_name', 'email', 'password1','password2']
 class CustomUserForm(UserCreationForm):
     pass 
+
+
+
+class EditProfileForm(UserChangeForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
+    is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
+    is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
+    date_joined = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model= User
+        fields=('username','first_name','last_name','email','password','last_login','is_superuser','is_staff','is_active','date_joined')
