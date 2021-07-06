76b6b6811849017b401228f0f93a3faf68cdae29
JeanContreras12 <j.contrerasmanriquez@uandresbello.edu>
2021-07-05 23:10:49 -0400

    re-inicio

---
+++ ProyectoDjango/urls.py
@@ -0,0 +1,47 @@
+"""ProyectoDjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from ColungaApp import views
+from pagina_01 import views as viewsindex
+from pagina_01.views import UserEditView
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('logeado/', login_required(viewsindex.login), name='logeado'),
+    path('registro/',login_required(viewsindex.registro), name='registro'),
+    path('accounts/login/',LoginView.as_view(template_name='ColungaApp/login.html'), name='login'),
+    path('',LogoutView.as_view(template_name='pagina_01/index.html'), name='inicio'),
+    path('planificador/',login_required(viewsindex.planificador), name='planificador'),
+    path('saladechat/',login_required(viewsindex.saladechat), name='saladechat'),
+    path('videoconferencias/',login_required(viewsindex.videoconferencia), name='videoconferencias'),
+    path('comunicados/',login_required(viewsindex.comunicados), name='comunicados'),
+    path('comunicado/',viewsindex.comunicadosINDEX, name='comunicados'),
+    path('organizaciones/',login_required(viewsindex.organizaciones), name='organizaciones'),
+    path('organizacionesADMIN/',login_required(viewsindex.organizacionesADMIN), name='organizaciones'),
+    path('perfil/',login_required(viewsindex.perfil), name='perfil'),
+
+    path('contacto/',login_required(viewsindex.contacto), name='contacto'),
+
+    path('edit_profile/',UserEditView.as_view(), name='edit_profile'),
+    path('password/',auth_views.PasswordChangeView.as_view()),
+    path('contacto/',login_required(viewsindex.contacto), name='contacto'),
+    path('login-admin/',login_required(viewsindex.loginADMIN), name='login-admin'),
+    path('planificadorADMIN/',login_required(viewsindex.planificadorAdmin), name='planificador'),
+]
---
+++ pagina_01/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1','password2']
+class CustomUserForm(UserCreationForm):
+    pass 
+
+
+
+class EditProfileForm(UserChangeForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    '''username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
+    is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
+    is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
+    date_joined = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
+    '''
+    class Meta:
+        model= User
+        fields=('first_name','last_name','email')
